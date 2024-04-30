50f0f0e8631eca916f38d39083148b4a9393c358
Usama <osamaimran25@gmail.com>
2024-04-30 11:47:35 +0500

    initial commit

---
+++ event_management/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from event_management.users.models import User
+from event_management.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
